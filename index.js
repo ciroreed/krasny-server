b358771f2fdcdd631811eb360717a81ccda289d1
ciroreed <ciroreed@gmail.com>
2015-10-30 00:44:15 +0100

    tested, solved some bugs

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 exports.build = function(configuration) {
+  var dirname = __dirname + '/../..';
   var express = require('express');
   var bodyParser = require('body-parser');
   var path = require('path');
   var app = express();
   var mapper = require('sqlite3_mapper').dbpath(configuration.dbpath);
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, configuration.publicdir)));
   if(configuration.reset){
     mapper.droptables(configuration.models);
   }
   mapper.createtables(configuration.models, function(entities){
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.static(path.join(dirname, configuration.publicdir)));
+    app.get('/', function(req, res){
+        res.sendFile(dirname + '/public/index.html');
+    });
     app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
@@ -18,25 +22,25 @@ exports.build = function(configuration) {
     });
     app.get('/:modelname', function(req, res){
       if(entities.indexOf(req.params.modelname) === -1) res.json({code: 1});
-      mapper.read({entity: req.entity, type: 'collection'}, function(arr){
+      mapper.read({entity: req.params.modelname, type: 'collection'}, function(arr){
         res.json(arr);
       });
     });
     app.post('/:modelname', function(req, res){
       if(entities.indexOf(req.params.modelname) === -1) res.json({code: 1});
-      mapper.create({entity: req.entity, subject: req.body}, function(){
+      mapper.create({entity: req.params.modelname, subject: req.body}, function(){
         res.json({code: 0});
       });
     });
     app.put('/:modelname/:id', function(req, res){
       if(entities.indexOf(req.params.modelname) === -1) res.json({code: 1});
-      mapper.update({entity: req.entity, subject: req.body, where: {id: req.params.id}}, function(){
+      mapper.update({entity: req.params.modelname, subject: req.body, where: {id: req.params.id}}, function(){
         res.json({code: 0});
       });
     });
     app.delete('/:modelname/:id', function(req, res){
       if(entities.indexOf(req.params.modelname) === -1) res.json({code: 1});
-      mapper.delete({entity: req.entity, where: {id: req.params.id}}, function(){
+      mapper.delete({entity: req.params.modelname, where: {id: req.params.id}}, function(){
         res.json({code: 0});
       });
     });
