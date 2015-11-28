932d542a0b4e211b4e7aca8c2e6b53b097a369a4
ciroreed <ciroreed@gmail.com>
2015-11-28 22:58:31 +0100

    code review

--- index.js
+++ index.js
@@ -15,31 +15,38 @@ exports.build = function(configuration) {
     app.get('/', function(req, res){
         res.sendFile(dirname + configuration.mainFile);
     });
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-      next();
+    app.use('/:modelname', function(req, res, next) {
+      if(entities.indexOf(req.params.modelname) === -1){
+        res.status(404);
+        res.end();
+      } else {
+        next();
+      }
+    });
+    app.use('/:modelname/*', function(req, res, next) {
+      if(entities.indexOf(req.params.modelname) === -1){
+        res.status(404);
+        res.end();
+      } else {
+        next();
+      }
     });
     app.get('/:modelname', function(req, res){
-      if(entities.indexOf(req.params.modelname) === -1) res.status(404).end();
       mapper.read({entity: req.params.modelname, type: 'collection'}, function(arr){
         res.json(arr);
       });
     });
     app.post('/:modelname', function(req, res){
-      if(entities.indexOf(req.params.modelname) === -1) res.status(404).end();
       mapper.create({entity: req.params.modelname, subject: req.body}, function(){
         res.json({code: 0});
       });
     });
     app.put('/:modelname/:id', function(req, res){
-      if(entities.indexOf(req.params.modelname) === -1) res.status(404).end();
       mapper.update({entity: req.params.modelname, subject: req.body, where: {id: req.params.id}}, function(){
         res.json({code: 0});
       });
     });
     app.delete('/:modelname/:id', function(req, res){
-      if(entities.indexOf(req.params.modelname) === -1) res.status(404).end();
       mapper.delete({entity: req.params.modelname, where: {id: req.params.id}}, function(){
         res.json({code: 0});
       });
